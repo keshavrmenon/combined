1624a35b6b9247dbcae1cd6c327672aa29255150
keshavrmenon <keshavmenon@iisc.ac.in>
2021-01-29 20:40:07 +0530

    edit

--- combinedr.py
+++ combinedr.py
@@ -6,18 +6,18 @@ f=TFile("higgsCombineTest.MultiDimFit.mH125.123456.root")
 f1=TFile("test.root", "RECREATE")
 t1 = f.Get("limit")
 
-h1 = TH1F("h_centre","", 100, -0.75, 1.85)
-h2 = TH1F("h_plus","", 100, -0.75, 1.85)
-h3 = TH1F("h_minus","", 100, -0.75, 1.85)
+h1 = TH1F("h_centre","r", 25, -0.75+1, 1.85+1)
+h2 = TH1F("h_plus","+rErr", 25, -0.75+1, 1.85+1)
+h3 = TH1F("h_minus","-rErr", 25, -0.75+1, 1.85+1)
 
 
 for entry in t1:
     if(entry.quantileExpected>0):
-        h2.Fill(entry.r-1)
+        h2.Fill(entry.r)
     elif(entry.quantileExpected == -1):
-        h3.Fill(entry.r-1)
+        h3.Fill(entry.r)
     else:
-        h1.Fill(entry.r-1)
+        h1.Fill(entry.r)
 
 h1.Write()
 h2.Write()
